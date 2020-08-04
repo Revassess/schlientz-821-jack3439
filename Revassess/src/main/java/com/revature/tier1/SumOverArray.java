d16002444a9e2d1118fb8728bbb828858be9e531
Doug Jackson <djack3439@yahoo.com>
2020-08-04 13:27:02 -0600

    updated parts 1 to 3 of tier 1 problem set

--- Revassess/src/main/java/com/revature/tier1/SumOverArray.java
+++ Revassess/src/main/java/com/revature/tier1/SumOverArray.java
@@ -3,6 +3,12 @@ package com.revature.tier1;
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
-		return 0;
+		int sum = 0;
+		if(arr != null) {
+			for(int i=0; i<arr.length; i++) {
+				sum = sum + arr[i];
+			}
+		}
+		return sum;
 	}
 }
--- Revassess/src/main/java/com/revature/tier1/User.java
+++ Revassess/src/main/java/com/revature/tier1/User.java
@@ -1,4 +1,22 @@
 package com.revature.tier1;
 
 public class User {
+	int id;
+	String firstName;
+	String lastName;
+	String username;
+	String password;
+	String role;
+	
+	public User(int id, String firstName, String lastName, String username, String password, String role) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
+		this.role = role;
+	}
+	
+	
 }
